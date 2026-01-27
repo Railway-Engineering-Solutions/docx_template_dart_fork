9fa495a43f6cced8c621ffa3579d514c4a641b75
Ian <79951156+joseianpatrick@users.noreply.github.com>
2026-01-27 14:00:49 +0800

    Update docx_template.dart

--- lib/docx_template.dart
+++ lib/docx_template.dart
@@ -1,5 +1,5 @@
 library docx_template;
 
-export 'src/template.dart' show DocxTemplate, TagPolicy;
+export 'src/template.dart' show DocxTemplate, TagPolicy, ImagePolicy;
 export 'src/model.dart';
 export 'src/tag_models.dart' show TagType, DocxTag, DocxTagCollection;
